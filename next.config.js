14dcd50a315c9a1b2963a286e4274b2b35c2ef80
Swikar Adhikari <swikara082@gmail.com>
2022-07-17 16:22:38 +0545

    config changes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  rules: {
+    "react/display-name": "off",
+  },
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
