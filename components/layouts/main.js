6909d200fa157feb9fa27beb8fc051fc14a357f7
Swikar Adhikari <swikara082@gmail.com>
2022-07-17 17:06:40 +0545

    fix: 🐛 Changed Favicon

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -10,7 +10,14 @@ const Main = ({ children, router }) => {
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Swikar Adhikari - Homepage</title>
+        <meta name="description" content="Swikar's Home Page" />
+        <meta name="author" content="Swikar Adhikari" />
+        <meta name="author" content="swikarr_" />
+        <link rel="apple-touch-icon" href="apple-touch-icon.png" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <meta property="og:site_name" content="Swikar Adhikari" />
+        <meta name="og:title" content="Swikar Adhikari" />
+        <meta property="og:type" content="website" />
       </Head>
 
       <Navbar path={router.asPath} />
