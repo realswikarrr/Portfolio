5947af081d6874ef7680f37d9738b5f6e0f93628
Swikar Adhikari <swikara082@gmail.com>
2022-07-17 15:13:37 +0545

    fix: 🐛 Fixed NavBar Focus Color

--- components/navbar.js
+++ components/navbar.js
@@ -16,16 +16,19 @@ import {
 } from "@chakra-ui/react";
 import { HamburgerIcon } from "@chakra-ui/icons";
 import ThemeToggleButton from "./theme-toggle-button";
+import { IoLogoGithub } from "react-icons/io5";
 
-const LinkItem = ({ href, path, children }) => {
+const LinkItem = ({ href, path, target, children, ...props }) => {
   const active = path === href;
   const inactiveColor = useColorModeValue("gray200", "whiteAlpha.900");
   return (
-    <NextLink href={href}>
+    <NextLink href={href} passHref scroll={false}>
       <Link
         p={2}
-        bg={active ? "glassTeal" : undefined}
+        bg={active ? "grassTeal" : undefined}
         color={active ? "#202023" : inactiveColor}
+        target={target}
+        {...props}
       >
         {children}
       </Link>
@@ -42,8 +45,8 @@ const Navbar = (props) => {
       as="nav"
       w="100%"
       bg={useColorModeValue("#ffffff40", "#20202380")}
-      style={{ backdropFilter: "blur(10px" }}
-      zIndex={1}
+      css={{ backdropFilter: "blur(10px)" }}
+      zIndex={2}
       {...props}
     >
       <Container
@@ -66,7 +69,7 @@ const Navbar = (props) => {
           width={{ base: "full", md: "auto" }}
           alignItems="center"
           flexGrow={1}
-          mt={{ base: 4, nmd: 0 }}
+          mt={{ base: 4, md: 0 }}
         >
           <LinkItem href="/works" path={path}>
             Works
@@ -78,8 +81,9 @@ const Navbar = (props) => {
 
         <Box flex={1} align="right">
           <ThemeToggleButton />
+
           <Box ml={2} display={{ base: "inline-block", md: "none" }}>
-            <Menu>
+            <Menu isLazy id="navbar-menu">
               <MenuButton
                 as={IconButton}
                 icon={<HamburgerIcon />}
